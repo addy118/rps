d0cbe561fc9a251f5d788f9bd946d9fea04bbaeb
addy <addyyy118@gmail.com>
2024-08-11 16:50:21 +0530

    Initial Commit

---
+++ ultimate-react-course/js-review/review.js
@@ -0,0 +1,205 @@
+const data = [
+  {
+    id: 1,
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  {
+    id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-01-01",
+    author: "Stanislaw Lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  {
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "해리 포터와 마법사의 돌",
+      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+      portuguese: "Harry Potter e a Pedra Filosofal",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  {
+    id: 5,
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: "왕좌의 게임",
+      polish: "Gra o tron",
+      portuguese: "A Guerra dos Tronos",
+      spanish: "Juego de tronos",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+function getBooks() {
+  return data;
+}
+
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+const book = getBook(2);
+
+// destructuring the objects
+const { id, title, genres, publicationDate } = book;
+console.log(id, title, genres);
+
+// destructuring the arrays
+const [primary, secondary] = genres;
+console.log(primary, secondary);
+
+// rest operator
+const [pri, sec, ...other] = genres;
+console.log(other);
+
+// spread operator for arrays
+updatedGenres = [...other, "adventure"];
+
+// spread operator for objects
+updatedBook = {
+  // spreads the properties and methods of book and inserts it
+  // instead of inserting it as a whole object
+  ...book,
+
+  // adding a new property
+  read: true,
+
+  // overwrites and updates the existing property
+  pages: 218,
+};
+
+// ternary operator
+const pageRanges =
+  updatedBook.pages > 1000 ? "over a thousand" : "less than a thousand";
+console.log(pageRanges);
+
+// arrow function
+const getYear = (str) => str.split("-")[0];
+console.log(getYear(publicationDate));
+
+// guard operator: prints the lhv if its falsy
+const guard = false && true
+console.log(guard)
+
+// default operator: prints the default value if lhv is falsy
+const defaultVal = false || true
+let count = 0
+const zero = count || 'no count'
+console.log(defaultVal, zero)
+
+// nullish coalescing operator: prints the default value if lhv is null or undefined
+const val = count ?? 'no count'
+console.log(val)
+
+// optional chaining: only accesses the property if the obj is defined
+function getTotalReviews(book) {
+  const goodReads = book.reviews.goodreads?.reviewsCount ?? 0;
+  const librarything = book.reviews.librarything?.reviewsCount ?? 0;
+  return goodReads + librarything
+}
+console.log(getTotalReviews(book))
